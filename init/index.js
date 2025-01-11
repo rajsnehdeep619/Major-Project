a8ca2683e9cca733e9f20ad843e839c4267c5fb9
Delta Student <rajsnehdeep8@gmail.com>
2025-01-11 15:58:34 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const Mongo_Url = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+  console.log("connected to db");
+})
+.catch((err)=>{
+  console.log(err);
+});
+async function main() {
+  await mongoose.connect(Mongo_Url);
+}
+
+const initDB = async()=>{
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+      ...obj,
+      owner: "677a01afc0ac3ff9703c4331",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initializes");
+};
+initDB();
